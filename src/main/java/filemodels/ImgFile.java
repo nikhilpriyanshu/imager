fae20c7e6aef351baace0be0184a01c3f18a12bc
Nikhil Priyanshu <nikhil.npriyanshu@gmail.com>
2020-05-18 23:07:23 +0530

    ImgFile class modified

---
+++ src/main/java/filemodels/ImgFile.java
@@ -0,0 +1,77 @@
+package filemodels;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Collection;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+
+public class ImgFile implements Serializable {
+    private static final long serialVersionUID = 1L;
+    File file;
+    Metadata metadata;
+
+    public ImgFile(File file) {
+        this.file = file;
+        try {
+            this.metadata = ImageMetadataReader.readMetadata(file);
+        } catch (ImageProcessingException | IOException e) {
+            // add log here
+        }
+    }
+
+    public Iterable<Directory> getDirectories() {
+        return metadata.getDirectories();
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    public boolean equals(ImgFile file) {
+        if (this.metadata.getDirectoryCount() != file.getMetadata().getDirectoryCount()) {
+            return false;
+        }
+        Iterable<Directory> fileDirectories = file.getMetadata().getDirectories();
+        for (Directory directory : this.metadata.getDirectories()) {
+            boolean dirFound = false;
+            for (Directory fileDirectory : fileDirectories) {
+                if (directory.getName().equals(fileDirectory.getName())) {
+                    dirFound = true;
+                    if (directory.getTagCount() != fileDirectory.getTagCount()) {
+                        return false;
+                    }
+                    Collection<Tag> tags = directory.getTags();
+                    Collection<Tag> fileTags = fileDirectory.getTags();
+                    for (Tag tag : tags) {
+                        boolean tagFound = false;
+                        for (Tag fileTag : fileTags) {
+                            if (tag.getTagName().equals(fileTag.getTagName())) {
+                                tagFound = true;
+                                if (!tag.getDescription().equals(fileTag.getDescription())) {
+                                    return false;
+                                }
+                            }
+                        }
+                        if(!tagFound) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            if (!dirFound) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
